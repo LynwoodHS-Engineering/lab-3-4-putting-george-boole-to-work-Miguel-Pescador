72e25304cd7955abb3be809fc1622fb20cb5b763
Miguel-Pescador <migpes2806@stu.mylusd.org>
2024-11-18 12:41:20 -0800

    Update Starter_while.ino FIXED

--- Starter_while.ino
+++ Starter_while.ino
@@ -10,18 +10,24 @@ void setup()
 {
     // Start Serial Moniter and set up pins
     Serial.begin(9600);
-    pinMode ();
-    pinMode ();
-    pinMode ();
+    pinMode (2, OUTPUT);
+    pinMode (3, OUTPUT);
+    pinMode (4, OUTPUT);
     
-    while (x < ) {
+    while (x <=3 ) {
         // Display the cycle number in the Serial Monitor
         Serial.print("Cycle no ");
         Serial.println(x);
         
         // Turn on each LED in sequence
-        digitalWrite ((x + 7), );
+        digitalWrite ((x + 7), HIGH);
+        Serial.print("LED");
+        Serial.print(x+1):
+        Serial.println(" is ON");
+        
         delay (2000)
+
+            digitalWrite(x+1,LOW)
         x = x + 1;
     }
 }
